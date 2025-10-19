567776ee0c47cde3b034842be754659e85a40ae8
Victor <vfurtado@tecnologia-unica.com.br>
2025-10-19 11:59:02 -0300

    Correção tracking

--- API/Models/AccessToken.cs
+++
@@ -1,8 +0,0 @@
-namespace API.Models
-{
-    public class AccessToken
-    {
-        public string Token { get; set; }
-        public DateTime ExpiresIn { get; set; }
-    }
-}
--- API/Models/Request/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace API.Models.Request
-{  
-    public class LoginRequest
-    {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-    }   
-}
